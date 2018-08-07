4a163fe4f717af90627cd51961af71f1ddf6b3c5
EdwardGoomba <ask@edwardvdesign.com>
2018-08-07 14:10:15 -0400

    Update scoreboard container

--- src/containers/Scoreboard.js
+++ src/containers/Scoreboard.js
@@ -1,23 +1,29 @@
-import React, { Component, PropTypes } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import * as PlayerActionCreators from '../actions/player';
-import Player from '../components/Player';
-import Header from '../components/Header';
-import AddPlayerForm from '../components/AddPlayerForm';
+import React, { Component, PropTypes } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import * as PlayerActionCreators from '../actions/player'
+import Player from '../components/Player'
+import Header from '../components/Header'
+import AddPlayerForm from '../components/AddPlayerForm'
 
 class Scoreboard extends Component {
   
   static propTypes = {
     players: PropTypes.array.isRequired
-  };
+  }
 
   render() {    
-    const { dispatch, players } = this.props;
-    const addPlayer = bindActionCreators(PlayerActionCreators.addPlayer, dispatch);
-    const removePlayer = bindActionCreators(PlayerActionCreators.removePlayer, dispatch);
-    const updatePlayerScore = bindActionCreators(PlayerActionCreators.updatePlayerScore, dispatch);
+    const { dispatch, players, selectedPlayerIndex } = this.props
+    const addPlayer = bindActionCreators(PlayerActionCreators.addPlayer, dispatch)
+    const removePlayer = bindActionCreators(PlayerActionCreators.removePlayer, dispatch)
+    const updatePlayerScore = bindActionCreators(PlayerActionCreators.updatePlayerScore, dispatch)
+    const selectPlayer = bindActionCreators(PlayerActionCreators.selectPlayer, dispatch)
     
+    let selectedPlayer
+    if(selectedPlayerIndex !== -1) {
+      selectedPlayer = players[selectedPlayerIndex]
+    }
+
     const playerComponents = players.map((player, index) => (
       <Player 
         index={index}
@@ -26,8 +32,9 @@ class Scoreboard extends Component {
         key={player.name}
         updatePlayerScore={updatePlayerScore}
         removePlayer={removePlayer}
+        selectPlayer={selectPlayer}
       />
-    ));
+    ))
     
     return (
       <div className="scoreboard">
@@ -36,18 +43,22 @@ class Scoreboard extends Component {
           { playerComponents }
         </div>
         <AddPlayerForm addPlayer={addPlayer} />
+        <div className='player-detail'>
+          <PlayerDetail selectedPlayer={selectedPlayer} />
+        </div>
       </div>
-    );
+    )
   }
 }
 
 const mapStateToProps = state => (
   {
-    players: state
+    players: state.players,
+    selectedPlayerIndex: state.selectedPlayerIndex
   }
-);
+)
 
-export default connect(mapStateToProps)(Scoreboard);
+export default connect(mapStateToProps)(Scoreboard)
 
 
 
